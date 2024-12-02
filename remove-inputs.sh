c3797f3ec8b47269f2f02062155ce5a1fd0fcd59
usysrc <usysrc@usysrc.dev>
2024-12-02 16:18:59 +0100

    remove all input files

---
+++ remove-inputs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+# if argument --delete is passed, then remove the files
+if [ "$1" = "--delete" ]; then
+    find . -name input -exec git rm {} \;
+# else echo a message
+else
+    # Remove all files named input from all subdirectories
+    # do a dry run first
+    find . -name input -exec echo {} \;
+    echo "To delete the files, pass --delete as an argument"
+fi
