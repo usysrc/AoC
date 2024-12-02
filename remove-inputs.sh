37966ecdfd0c8e19211972868920eee1651a8dde
usysrc <usysrc@usysrc.dev>
2024-12-02 16:20:35 +0100

    Enhance remove-inputs.sh with usage instructions and clarify dry run behavior

--- remove-inputs.sh
+++ remove-inputs.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+# Remove all files named input from all subdirectories
+# Usage: ./remove-inputs.sh --delete
+# The --delete argument is optional
 
 # if argument --delete is passed, then remove the files
 if [ "$1" = "--delete" ]; then
     find . -name input -exec git rm {} \;
 # else echo a message
 else
-    # Remove all files named input from all subdirectories
-    # do a dry run first
+    # do a dry run
     find . -name input -exec echo {} \;
     echo "To delete the files, pass --delete as an argument"
 fi
